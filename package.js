a81dfc1ed93340317d09851c25e841945b325d4b
Jimmie Butler <jimmiebtlr@gmail.com>
2015-11-16 10:25:57 -0600

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:rest-sync',
-  version: '0.0.1-rc.1',
+  version: '0.0.1-rc.2',
   // Brief, one-line summary of the package.
   summary: 'Restful data synchronization for meteor applications.',
   // URL to the Git repository containing the source code for this package.
